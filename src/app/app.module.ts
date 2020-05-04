3e33498212ff46ad1ec3daad0064472b90dde047
Ramin A <rahmadi@knosys.it>
2020-05-04 22:28:07 +1000

    Adding details-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsFormComponent } from './details-form/details-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsFormComponent
   ],
   imports: [
     BrowserModule,
@@ -17,3 +19,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/details-form/details-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-form',
+  templateUrl: './details-form.component.html',
+  styleUrls: ['./details-form.component.scss']
+})
+export class DetailsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
