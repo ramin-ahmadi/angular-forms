9be9cfc73b2dea4f13b55047dce18183fdc0b87f
Ramin A <rahmadi@knosys.it>
2020-05-15 23:19:21 +1000

    Add observable to postUserDetails method

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,12 +1,18 @@
 import { Person } from './../data-model/person.model';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  postUserDetails(person: Person){}
+  postUserDetails(person: Person): Observable<Person>{
+    return this.http.post<Person>('ApiUrl', person)
+  }
 }
